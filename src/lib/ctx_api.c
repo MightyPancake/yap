83ec3f4f939ca208679e796f5261876a77223574
Filip Król <mightypancake@gmail.com>
2026-06-22 23:02:10 +0200

    Grammar return statement fix, dynamic type impl doc (for later) and ctx_api skelly

---
+++ src/lib/ctx_api.c
@@ -0,0 +1,9 @@
+#include "yap/all.h"
+
+// void yap_error(yap_ctx* ctx, char* msg){
+//     yap_error err = (yap_error){
+//         .kind = yap_error_no_pos,
+//         .msg = msg
+//     };
+//     yap_ctx_push_error(ctx, err);
+// }
